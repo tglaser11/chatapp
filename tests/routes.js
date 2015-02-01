254d22552ed48fcd609f0a4d64a544e86d822926
Tom Glaser <tglaser>
2015-02-01 11:44:50 -0600

    Added Dev Dependencies

---
+++ tests/routes.js
@@ -0,0 +1,20 @@
+/**
+ * Created by tom on 2/1/15.
+ */
+
+
+var routes = require('../routes'),
+    config = require('../config'),
+    nodeunit = require('nodeunit'),
+    Request = require('./request'),
+    Response = require('./response');
+
+exports.indexRouteTest = function(test){
+    var res = new Response();
+    test.equal(res.view, undefined);
+    routes.index({}, res);
+    test.equal(res.view, 'index');
+    test.equal(res.viewData.title, 'Index');
+    test.done();
+};
+
