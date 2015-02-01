5c1c97b952f7b5aae09aa8326cba2327e3335a3a
Tom Glaser <tfglaser@gmail.com>
2015-02-01 00:41:15 +0000

    changes to add chatworker app to docker scripts

--- resetdockerps.sh
+++ resetdockerps.sh
@@ -2,7 +2,9 @@ sudo docker stop chatapp
 sudo docker stop redis
 sudo docker stop rabbitmq
 sudo docker stop logworker
+sudo docker stop chatworker
 sudo docker rm chatapp
 sudo docker rm redis
 sudo docker rm rabbitmq
 sudo docker rm logworker
+sudo docker rm chatworker
